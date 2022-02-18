748867534c20dc19b736a2b3f256728e345a086d
Gabriel Alexandre <gabriel.shaad@gmail.com>
2022-02-17 22:27:53 -0300

    Add forms and mocked transactions

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 import { App } from "./App";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freela de React",
+          type: "deposit",
+          category: "Dev",
+          amount: 8000,
+          createdAt: "2021-01-25T12:00:00.000Z",
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1500,
+          createdAt: "2021-01-28T12:00:00.000Z",
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     });
   },
 });
